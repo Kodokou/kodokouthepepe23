3c0d276fa8f2aa036c26c29290e99ebb3d98c810
Xspeed <xspeedpl@windowslive.com>
2017-01-05 21:10:19 +0100

    Initial buggy version of Finger of Zeus

---
+++ Promethium/Items/Weapons/FingerofZeus.cs
@@ -0,0 +1,38 @@
+﻿using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+
+namespace Promethium.Items.Weapons
+{
+    class FingerofZeus : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Finger of Zeus";
+            item.damage = 40;
+            item.magic = true;
+            item.mana = 5;
+            item.width = 40;
+            item.height = 40;
+            item.toolTip = "+50% damage during rain";
+            item.useTime = 10;
+            item.useAnimation = 10;
+            item.useStyle = 5;
+            Item.staff[item.type] = true;
+            item.noMelee = true;
+            item.knockBack = 2;
+            item.value = 10000;
+            item.rare = 8;
+            item.UseSound = SoundID.Item20;
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("LightningStrike");
+            item.shootSpeed = 120;
+        }
+
+        public override DrawAnimation GetAnimation()
+        {
+            return new DrawAnimationVertical(10, 3);
+        }
+    }
+}
---
+++ Promethium/Projectiles/LightningStrike.cs
@@ -0,0 +1,93 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace Promethium.Projectiles
+{
+    class LightningStrike : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Lightning Strike";
+            projectile.width = 8;
+            projectile.height = 8;
+            projectile.friendly = true;
+            projectile.timeLeft = 4;
+            Main.projFrames[projectile.type] = 1;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Vector2 origin = new Vector2(projectile.ai[0], projectile.ai[1]);
+            Vector2 pos = projectile.Center;
+            CreateBolt(origin, pos);
+            base.Kill(timeLeft);
+        }
+
+        private void CreateBolt(Vector2 source, Vector2 dest)
+        {
+            Vector2 tangent = dest - source;
+            Vector2 normal = Vector2.Normalize(new Vector2(tangent.Y, -tangent.X));
+            float length = tangent.Length();
+
+            var positions = new System.Collections.Generic.List<float>();
+            positions.Add(0);
+
+            for (int i = 0; i < length / 4; i++) positions.Add(Rand(0, 1));
+
+            positions.Sort();
+
+            const float Sway = 80;
+            const float Jaggedness = 1 / Sway;
+
+            Vector2 prevPoint = source;
+            float prevDisplacement = 0;
+            for (int i = 1; i < positions.Count; i++)
+            {
+                float pos = positions[i];
+
+                // used to prevent sharp angles by ensuring very close positions also have small perpendicular variation.
+                float scale = (length * Jaggedness) * (pos - positions[i - 1]);
+
+                // defines an envelope. Points near the middle of the bolt can be further from the central line.
+                float envelope = pos > 0.95f ? 20 * (1 - pos) : 1;
+
+                float displacement = Rand(-Sway, Sway);
+                displacement -= (displacement - prevDisplacement) * (1 - scale);
+                displacement *= envelope;
+
+                Vector2 point = source + pos * tangent + displacement * normal;
+                NewLineAt(prevPoint, point);
+                prevPoint = point;
+                prevDisplacement = displacement;
+            }
+            NewLineAt(prevPoint, dest);
+        }
+
+        private static float Rand(float min, float max)
+        {
+            return (float)Main.rand.NextDouble() * (max - min) + min;
+        }
+
+        private void NewLineAt(Vector2 start, Vector2 end)
+        {
+            /*
+                const float ImageThickness = 8;
+                float thicknessScale = Thickness / ImageThickness;
+
+                Vector2 capOrigin = new Vector2(t1.Width, t1.Height / 2f);
+                Vector2 middleOrigin = new Vector2(0, t2.Height / 2f);
+                Vector2 middleScale = new Vector2(tangent.Length(), thicknessScale);
+
+                spriteBatch.Draw(t2, start, null, tint, theta, middleOrigin, middleScale, SpriteEffects.None, 0f);
+            */
+            Vector2 tangent = end - start;
+            float theta = (float)System.Math.Atan2(tangent.Y, tangent.X);
+            int dust = Dust.NewDust(start, 2, 2, DustID.WitherLightning);
+            Dust d = Main.dust[dust];
+            d.rotation = theta;
+            d.noGravity = true;
+        }
+    }
+}
