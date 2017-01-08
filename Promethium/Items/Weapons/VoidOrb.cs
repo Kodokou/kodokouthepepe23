23c9680499b7ca973461bab80f7814691026e423
Xspeed <xspeedpl@windowslive.com>
2017-01-08 02:14:55 +0100

    Added a throwable projectile for the Vortex

---
+++ Promethium/Items/Weapons/VoidOrb.cs
@@ -0,0 +1,32 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+
+namespace Promethium.Items.Weapons
+{
+	public class VoidOrb : ModItem
+	{
+		public override void SetDefaults ()
+		{
+			item.damage = 10;
+			item.name = "Void Orb";
+            item.width = 32;
+            item.height = 32;
+			item.toolTip = "Spawns a vortex that draws in nearby enemies";
+			item.shoot = mod.ProjectileType("VoidOrb");
+			item.shootSpeed = 10;
+			item.magic = true;
+			item.useStyle = 1;
+			item.useTime = 24;
+			item.useAnimation = 24;
+            item.mana = 50;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.autoReuse = false;
+            item.knockBack = 6;
+            item.value = Item.buyPrice(0, 2);
+            item.rare = 6;
+            item.UseSound = Terraria.ID.SoundID.Item1;
+        }
+	}
+}
+
---
+++ Promethium/Projectiles/VoidOrb.cs
@@ -0,0 +1,36 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace Promethium.Projectiles
+{
+    class VoidOrb : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.name = "Void Orb";
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.aiStyle = 2;
+            projectile.friendly = true;
+            projectile.timeLeft = 32;
+            projectile.ignoreWater = true;
+            Main.projFrames[projectile.type] = 1;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            if (Main.myPlayer == projectile.owner)
+            {
+                int vortexId = mod.ProjectileType("Vortex");
+                for (int i = 0; i < 200; ++i)
+                {
+                    Projectile p = Main.projectile[i];
+                    if (p.owner == projectile.owner && p.type == vortexId)
+                        p.Kill();
+                }
+                Projectile.NewProjectile(projectile.Center - Vector2.UnitY * 24, Vector2.Zero, vortexId, projectile.damage, projectile.knockBack, projectile.owner);
+            }
+        }
+    }
+}
