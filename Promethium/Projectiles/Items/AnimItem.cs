328a36c154ea6fa1ff5a2439033704cf22b88a2d
Nopezal <nop@localhost.localdomain>
2017-01-13 00:46:07 +0100

    AnimItem is now usable

--- Promethium/Projectiles/Items/AnimItem.cs
+++ Promethium/Projectiles/Items/AnimItem.cs
@@ -1,22 +1,37 @@
 ﻿using Terraria.ModLoader;
 using Terraria;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Promethium.Projectiles.Items
 {
     public abstract class AnimItem : ModProjectile
     {
+        public int frames;
+        public Texture2D texture;
+
+        public virtual void Initialize() { }
+        public virtual void Animate()
+        {
+            if (frames != 1 && projectile.ai[0]++ % 10 == 0)
+                projectile.frame += projectile.ai[0] / 10 % frames * texture.Height;
+        }
+        public virtual void RealAI() { }
+
         public override void SetDefaults()
         {
-            projectile.width = 40;
-            projectile.height = 40;
-            projectile.aiStyle = 20;
+            Initialize();
             projectile.friendly = true;
-            projectile.penetrate = -1;
-            projectile.tileCollide = false;
-            projectile.ownerHitCheck = true;
-            Main.projFrames[projectile.type] = 3;
+            texture = mod.GetTexture("Projectiles/" + projectile.name);
+            frames = texture.Height / projectile.height;
+            Main.projFrames[projectile.type] = frames;
         }
-
-        public override bool CanDamage() { return false; }
+        //removed CanDamage because spears and other melee projectiles
+        public override void AI()
+        {
+            Animate();
+            RealAI();
+            if (!projectile.aiStyle <= 0)
+                base.AI();
+        }
     }
 }
