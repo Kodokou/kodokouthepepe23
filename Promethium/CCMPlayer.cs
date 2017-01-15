1b677fea1662102468aa209697c88c96e1fc5fa0
Nopezal <nop@localhost.localdomain>
2017-01-15 21:18:32 +0100

    actual Mana Buckler commit fml

--- Promethium/CCMPlayer.cs
+++ Promethium/CCMPlayer.cs
@@ -1,12 +1,14 @@
 ﻿using Terraria.ModLoader;
 using Terraria;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Promethium
 {
     class CCMPlayer : ModPlayer
     {
         private const byte KEY_DOWN = 0, KEY_UP = 1, KEY_RIGHT = 2, KEY_LEFT = 3;
+        public int manaBucklerLeft = 0, time = 0;
 
         public override void SetControls()
         {
@@ -21,6 +23,12 @@ namespace Promethium
             }
         }
 
+        public override void ResetEffects()
+        {
+            if (player.FindBuffIndex(mod.BuffType("ManaBuckler")) == -1)
+                manaBucklerLeft = 0;
+        }
+
         private void KeyDoubleTap(int key)
         {
             //if (key == (Main.ReversedUpDownArmorSetBonuses ? KEY_UP : KEY_DOWN))
@@ -32,5 +40,67 @@ namespace Promethium
             //if (key == (Main.ReversedUpDownArmorSetBonuses ? KEY_UP : KEY_DOWN))
             //    if (time >= 60) player.MinionRestTargetPoint = Vector2.Zero;
         }
+
+        public override void ModifyHitByNPC(NPC npc, ref int damage, ref bool crit)
+        {
+            int diff = player.statLifeMax2 - player.statLife, buffIndex = player.FindBuffIndex(mod.BuffType("ManaBuckler"));
+            if (0 < diff && !(player.statMana - diff * 2 < 0) && manaBucklerLeft > 0)
+            {
+                manaBucklerLeft--;
+                player.statMana -= diff * 2;
+                player.statLife += diff;
+            }
+            if (player.statMana - diff * 2 < 0 || manaBucklerLeft < 1)
+            {
+                player.buffTime[buffIndex] = 0;
+            }   
+        }
+
+        public override void ModifyHitByProjectile(Projectile proj, ref int damage, ref bool crit)
+        {
+            ModifyHitByNPC(null, ref damage, ref crit);
+        }
+
+        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
+        {
+            if (player.HeldItem.type == mod.ItemType("GauntletofEmbers"))
+                drawInfo.drawHands = false;
+        }
+
+        public override void ModifyDrawLayers(System.Collections.Generic.List<PlayerLayer> layers)
+        {
+            frontLayer.visible = true;
+            layers.Add(frontLayer);
+        }
+
+        public override void PostUpdate()
+        {
+            if (++time >= 256)
+                time = 0;
+        }
+
+        public static readonly PlayerLayer frontLayer = new PlayerLayer("Promethium", "CCM Front Layer", PlayerLayer.MiscEffectsFront, drawInfo =>
+            {
+                
+                Mod mod = ModLoader.GetMod("Promethium");
+                Player plr = drawInfo.drawPlayer;
+                CCMPlayer mplr = plr.GetModPlayer<CCMPlayer>(mod);
+                if (mplr.manaBucklerLeft > 0)
+                {
+                    
+                    Texture2D tex = mod.GetTexture("Items/Weapons/ManaBuckler");
+                    Vector2 pos = plr.MountedCenter + Vector2.UnitX * 8 * plr.direction;
+                    float step = System.Math.Abs(mplr.time - 128) / 128F;
+                    float scale = step * 0.2F + 0.9F;
+                    Color c = Lighting.GetColor((int)(pos.X / 16F), (int)(pos.Y / 16F)) * (step * 0.5F + 0.25F);
+                    pos -=  Main.screenPosition;
+                    var data = new Terraria.DataStructures.DrawData(tex, pos, null, c, 0, tex.Size() / 2 * scale, scale, SpriteEffects.None, 0);
+                    Main.playerDrawData.Add(data);
+
+                    // TODO: Maybe some effects IDK, that's how you do them tho
+                    //int dust = Dust.NewDust();
+                    //Main.playerDrawDust.Add(dust); // Important apparently
+                }
+            });
     }
 }
---
+++ Promethium/Items/Weapons/ManaBuckler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace Promethium.Items.Weapons
+{
+    public class ManaBuckler : ModItem 
+    {
+        public override void SetDefaults()
+        {
+            item.name = "Mana Buckler";
+            item.width = 22;
+            item.height = 34;
+            item.useStyle = 5;
+            item.useAnimation = 22;
+            item.useTime = 22;
+            item.value = Item.buyPrice(0, 2);
+            item.mana = 10;
+            item.rare = 3;
+            item.toolTip = "Mana becomes shield";
+            item.UseSound = SoundID.Item8;
+        }
+
+        public override bool UseItem(Player plr)
+        {
+            plr.GetModPlayer<CCMPlayer>(mod).manaBucklerLeft = 5;
+            plr.AddBuff(mod.BuffType("ManaBuckler"), 7200);
+            return true;
+        }
+    }
+}
+
